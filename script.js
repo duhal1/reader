f3d3a83ba21866b7ca7ed5440eb3c77fb889a5a0
duhal1 <duhal123456789@gmail.com>
2024-05-05 19:44:40 +1100

    создал read html и read css

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// const newLocation = (text) => {
+//     window.location.href = "read.html"
+//     h1 = document.querySelector("h1");
+//     p = document.querySelector("p");
+//     // h1.innerHTML = name;
+//     p.innerHTML = text;
+// }   
+
 const book = class {
     width = 100; height = 100
     constructor(text, name, img) {
@@ -7,12 +15,25 @@ const book = class {
         this.width = this.width;
         this.height = this.height;
     }
+
+    newLocation = () => {
+        window.location.href = "read.html"
+        h1 = document.querySelector("h1");
+        p = document.querySelector("p");
+        h1.innerHTML = this.name;
+        p.innerHTML = this.text;
+    }   
+    
+
     createBook = () => {
         const allBooks = document.querySelector(".allBooks");
-        allBooks.innerHTML += `<div class = "${this.name}"><img width="${this.width} height="${this.height} src=${this.img}><p>${this.name}</p></div>`;
+        allBooks.innerHTML += `<div  class = "${this.name}"><img width="${this.width} height="${this.height} src=${this.img}><p>${this.name}</p></div>`;
         const book = document.querySelector(`.${this.name}`)
-        book.style.margin = "10px"
-        book.click(() => {window.location.href='read.html'})
+        book.style.margin = "10px";
+        book.click(() => {
+            this.newLocation
+        })
+
     }
 }
 
