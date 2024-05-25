bc8f23ec6bc89f9440043b389884ceaefb33fcfd
duhal1 <duhal123456789@gmail.com>
2024-05-25 16:43:03 +1100

    последняя версия

--- script.js
+++ script.js
@@ -8,13 +8,17 @@ class book  {
     }
 
     createBook ()  {
-        const allBooks = document.querySelector(".all-books");
-        allBooks.innerHTML += `<div><a href="books/read.html?${this.textNum}"><img src = ${this.img}></a><p>${this.name}</p></div>`;
+        const allBooks = document.querySelector('.all-books');
+
+        allBooks.innerHTML += `<div><a href="books/read.html?${this.textNum}">
+        <img src = ${this.img}></a>
+        <p>${this.name}</p></div>`;
     }
 }
 
 names.forEach((element, i) => {
-            let book1 = new book(textNum = i, name = element, img = images[i])
+        let book1 = new book(textNum = i, name = element, img = images[i])
         book1.createBook();
-    })
+    }
+)
 
